823f6582435ec7139511d89c824769337f8030cd
OT13 <omertahir13@gmail.com>
2020-11-01 14:19:39 +0800

    Created a nested LinearView with two buttons. Populated the question and two answer buttons. Both buttons utilizing the setOnClickListener to listen for clicks from either button

--- app/src/main/java/com/ooteedemo/trueglobalcitizenquiz/MainActivity.java
+++ app/src/main/java/com/ooteedemo/trueglobalcitizenquiz/MainActivity.java
@@ -3,12 +3,39 @@ package com.ooteedemo.trueglobalcitizenquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button trueBtn;
+    private Button falseBtn;
+    private TextView questionTxtVw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        trueBtn = findViewById(R.id.true_btn);
+        falseBtn = findViewById(R.id.false_btn);
+        questionTxtVw = findViewById(R.id.answer_text_view);
+
+//        Register our buttons to listen to click events using the OnClickListener interface
+        trueBtn.setOnClickListener(this);
+        falseBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.true_btn:
+                Toast.makeText(MainActivity.this,"True",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.false_btn:
+                Toast.makeText(MainActivity.this,"False",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
