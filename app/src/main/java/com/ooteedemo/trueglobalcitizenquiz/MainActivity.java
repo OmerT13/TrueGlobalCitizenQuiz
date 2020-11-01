a4eb6397c80524d1cc1937c06825dfbc7c67be11
OT13 <omertahir13@gmail.com>
2020-11-01 18:15:11 +0800

    Created the Next button and coded it so it loops through the question bank

--- app/src/main/java/com/ooteedemo/trueglobalcitizenquiz/MainActivity.java
+++ app/src/main/java/com/ooteedemo/trueglobalcitizenquiz/MainActivity.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button trueBtn;
     private Button falseBtn;
+    private ImageButton nextBtn;
+
     private TextView questionTxtVw;
+
+    private int currentQuestionIndex = 0;
 
     private Question[] questionsBank = new Question[] {
             new Question(R.string.question_brazil,true),
@@ -30,11 +35,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         trueBtn = findViewById(R.id.true_btn);
         falseBtn = findViewById(R.id.false_btn);
+        nextBtn = findViewById(R.id.next_btn);
+
         questionTxtVw = findViewById(R.id.answer_text_view);
 
 //        Register our buttons to listen to click events using the OnClickListener interface
         trueBtn.setOnClickListener(this);
         falseBtn.setOnClickListener(this);
+        nextBtn.setOnClickListener(this);
     }
 
     @Override
@@ -45,6 +53,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.false_btn:
                 Toast.makeText(MainActivity.this,"False",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.next_btn:
+                Log.d("MYTAG", "onClick: "+currentQuestionIndex);
+                questionTxtVw.setText(questionsBank[currentQuestionIndex].getAnswerResId());
+//                We use the modulus to make sure we loop the array and never go out of bounds
+                currentQuestionIndex = (currentQuestionIndex+1)%questionsBank.length;
                 break;
         }
     }
